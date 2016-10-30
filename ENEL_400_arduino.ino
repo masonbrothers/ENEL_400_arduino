4a2c6010632f2e208795e1700dbdbcf1be29790b
Mason Brothers <brothers.mason@gmail.com>
2016-10-30 12:12:09 -0600

    We need to test now

--- ENEL_400_arduino.ino
+++ ENEL_400_arduino.ino
@@ -1,40 +1,226 @@
+// For Water Temperature Probe
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include "DHT.h" //Temperature and Humidity Sensor
 
-#include <SPI.h>
+// For Air Temperature and Humidity Sensor
+#include "DHT.h" 
+
+// For SD Card
+#include <SPI.h> 
 #include <SD.h>
 
+// For Real Time Clock (RTC)
+#include <Wire.h>
+#include "RTClib.h"
 
-#define BUZZER_PIN                                        5
-#define PUMP_PIN                                          4
+#include <Adafruit_Sensor.h>
+#include "Adafruit_TSL2591.h"
+
+#define BUZZER_PIN                                        4 // ???
+#define PUMP_PIN                                          3
 #define ESP8266_TX_ARDUINO_RX_PIN                         
-#define ESP8266_RX_ARDUINO_TX                             
-#define ESP8266_RESET                                     
-#define WATER_THERMOMETER_DALLAS_ONE_WIRE_PIN             2
-#define AMBIENT_THERMOMETER_DALLAS_ONE_WIRE_PIN           
-#define WATER_LEVEL_ANALOG_PIN                            A0
+#define ESP8266_RX_ARDUINO_TX_PIN                             
+#define ESP8266_RESET_PIN                                     
+#define WATER_THERMOMETER_DALLAS_ONE_WIRE_PIN             3 // ???
+#define AMBIENT_THERMOMETER_DALLAS_ONE_WIRE_PIN           4 // ???
+#define WATER_LEVEL_ANALOG_PIN                            A1 // ???
 #define WATER_LEVEL_RESISTOR_VALUE                        560 // Value of the resistor added in ohms    
-
-// What pin to connect the sensor to
-#define WATER_LEVEL_ANALOG_PIN                            A0 
 #define MAX_RESOLUTION                                    1023
 #define ADAFRUIT_DATA_LOGGING_SHIELD_CHIP_SELECT          10
 // Note that this pin needs a 10K pullup Resistor 
-#define DHTPIN                                            3
-#define DHTTYPE                                           DHT22
-#define LUMINOSITY_I2C_PIN                                    
+#define AMBIENT_TEMPERATURE_HUMIDITY_SENSOR_PIN           5 // ???
+#define AMBIENT_TEMPERATURE_HUMIDITY_SENSOR_TYPE          DHT22
+#define LUMINOSITY_SCL_PIN                                A5
+#define LUMINOSITY_SDA_PIN                                A4
+#define DATA_FILE_NAME                                    "AQUA_DATA.TXT"
+#define LOG_FILE_NAME                                     "AQUA_LOG.TXT"
+#define TSL2591_NUMBER                                    2591
 
 OneWire oneWire(WATER_THERMOMETER_DALLAS_ONE_WIRE_PIN);
 
-DallasTemperature sensors(&oneWire);
+DallasTemperature waterTemperatureSensor(&oneWire);
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(AMBIENT_TEMPERATURE_HUMIDITY_SENSOR_PIN, AMBIENT_TEMPERATURE_HUMIDITY_SENSOR_TYPE);
 
-float AmbientTemperature;
-float AmbientHumidity;
+RTC_PCF8523 rtc;
+
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(TSL2591_NUMBER);
+
+struct AmbientTemperatureHumidity
+{
+  float ambientTemperature;
+  float ambientHumidity;
+  boolean valid;
+};
+
+
 boolean validAmbientTemperatureHumidity = true; //Boolean to detect whether or not Reading was valid
 
+void setup() {
+  Serial.begin(9600);
+  
+  setupBuzzer();
+  stopBuzzer();
+  
+  setupPump();
+  startPump();
+
+  setupLightSensor();
+  
+  setupWaterTemperatureSensor();
+  if (setupSDCard())
+    Serial.println("Initialized SD Card.");
+  else
+    Serial.println("Failed to initialize SD Card.");
+
+  setupAmbientTemperatureAndHumidity();
+  
+  if (setupRTC())
+  {
+    Serial.println("Initialized the RTC.");
+    setRTCTime();
+  }
+  else
+  {
+    Serial.println("Failed to initialize the RTC.");
+    writeToSDCardLog("Failed to initialize the RTC.");
+  }
+}
+
+void loop() {
+  
+  delay(2000);
+
+  Serial.println("Water Temp:\t" + (String)getWaterTemperature() + "degC");
+
+  AmbientTemperatureHumidity ambientTemperatureHumidity = readAmbientTemperatureAndHumidity();
+  if (ambientTemperatureHumidity.valid)
+  {
+    Serial.println((String)ambientTemperatureHumidity.ambientTemperature + "degC\t" + (String)ambientTemperatureHumidity.ambientHumidity + "%");
+  }
+  else
+  {
+    Serial.println("Ambient Temperature and Humidity Sensor Not Working!!!");
+    writeToSDCardLog("Ambient Temperature and Humidity Sensor Not Working!!!");
+  }
+
+  Serial.println(getRTCStringTime());
+  
+  if (!writeToSDCard(DATA_FILE_NAME, "Greatness!!!"))
+  {
+    Serial.println("Cannot write to " + (String)DATA_FILE_NAME);
+  }
+
+  Serial.println("Visible light: " + (String)getVisibleLight() + " lux");
+  
+  Serial.println("IR light: " + (String)getIRLight() + " lux");
+  
+  
+}
+
+void setupLightSensor()
+{
+  tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);
+}
+
+int getVisibleLight()
+{
+  return tsl.getLuminosity(TSL2591_VISIBLE);
+}
+
+int getIRLight()
+{
+  return tsl.getLuminosity(TSL2591_INFRARED);
+}
+
+bool writeToSDCardLog(String input)
+{
+  if (writeToSDCard(LOG_FILE_NAME, input))
+    return true;
+  Serial.println("Cannot write to " + (String)LOG_FILE_NAME);
+  return false;
+}
+
+void setupWaterTemperatureSensor()
+{
+  waterTemperatureSensor.begin();
+}
+
+float getWaterTemperature()
+{
+  waterTemperatureSensor.requestTemperatures();
+  return waterTemperatureSensor.getTempCByIndex(0);
+}
+
+boolean setupRTC()
+{
+  //rtc.initialized();
+  return rtc.begin();
+}
+
+void setRTCTime()
+{
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+}
+
+DateTime getRTCTime()
+{
+  return rtc.now();
+}
+
+String getRTCStringTime()
+{
+  DateTime now = getRTCTime();
+  String output = (int)now.year() + (String)"-" + addPrecedingZero(now.dayOfTheWeek()) + (String)"-" + addPrecedingZero(now.day()) + (String)"T" + addPrecedingZero(now.hour()) + (String)":" + addPrecedingZero(now.minute()) + (String)":" + addPrecedingZero(now.second());
+  return output;
+}
+
+String addPrecedingZero(int input)
+{
+  String output;
+  if (input > 10)
+    output = (String) input;
+  else
+    output = "0" + (String) input;
+  return output;
+}
+
+boolean setupSDCard()
+{
+  // see if the card is present and can be initialized:
+  if (!SD.begin(ADAFRUIT_DATA_LOGGING_SHIELD_CHIP_SELECT)) {
+    return false;
+  }
+  return true;
+}
+
+void setupAmbientTemperatureAndHumidity()
+{
+  dht.begin();
+}
+
+
+AmbientTemperatureHumidity readAmbientTemperatureAndHumidity()
+{
+  //Sensor requires a delay of atleast 2 seconds to work
+  delay(2000);
+  AmbientTemperatureHumidity ambientTemperatureHumidity;
+  ambientTemperatureHumidity.ambientTemperature = dht.readTemperature();
+  ambientTemperatureHumidity.ambientHumidity = dht.readHumidity();
+  
+  //If Ambient Temperature and Humidity Readings are garbage output an error message
+  if (isnan(ambientTemperatureHumidity.ambientTemperature) || isnan(ambientTemperatureHumidity.ambientHumidity))
+  {
+    validAmbientTemperatureHumidity = false;
+  }
+  else
+  {
+    ambientTemperatureHumidity.valid = true;
+  }
+
+  return ambientTemperatureHumidity;
+} 
 
 float waterLevelSensorResistance()
 {
@@ -45,86 +231,33 @@ float waterLevelSensorResistance()
   return value;
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  /*
+void setupBuzzer()
+{
   pinMode(BUZZER_PIN, OUTPUT);
-  stopBuzzer();
-  pinMode(PUMP_PIN, OUTPUT);
-  startPump();
-  */
-  sensors.begin();
-  Serial.begin(9600);
-
-  setupSDCard();
-
-  dht.begin();
-
-  
 }
 
-boolean setupSDCard()
+void startBuzzer()
 {
-  Serial.print("Initializing SD card...");
-  
-  // see if the card is present and can be initialized:
-  if (!SD.begin(ADAFRUIT_DATA_LOGGING_SHIELD_CHIP_SELECT)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    return false;
-  }
-  Serial.println("card initialized.");
-  return true;
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  sensors.requestTemperatures();
-  Serial.print((String)sensors.getTempCByIndex(0) + "\n");
-  delay(1000);
-  ReadAmbientTemperatureAndHumidity();
-  Serial.println((String)AmbientTemperature + "degC\t" + (String)AmbientHumidity + "%");
-  writeToSDCard("Aqua.txt", "Greatness!!!");
-}
-
-
-
-
-void ReadAmbientTemperatureAndHumidity()
-{
-  //Sensor requires a delay of atleast 2 seconds to work
-  delay(2000);
-  AmbientTemperature = dht.readTemperature();
-  AmbientHumidity = dht.readHumidity();
-  
-  //If Ambient Temperature and Humidity Readings are garbage output an error message
-  if (isnan(AmbientTemperature) || isnan(AmbientHumidity))
-  {
-    Serial.println("Ambient Temperature and Humidity Sensor Failed!");
-    validAmbientTemperatureHumidity = false;
-    return;
-  }
-} 
-
-
-/*
-bool saveData(SensorData sensorData) {
-  
-}
-*/
-void startBuzzer() {
   digitalWrite(BUZZER_PIN, HIGH);
 }
 
-void stopBuzzer() {
+void stopBuzzer()
+{
   digitalWrite(BUZZER_PIN, LOW);
 }
 
-void startPump() {
+void setupPump()
+{
+  pinMode(PUMP_PIN, OUTPUT);
+}
+
+void startPump()
+{
   digitalWrite(PUMP_PIN, HIGH);
 }
 
-void stopPump() {
+void stopPump()
+{
   digitalWrite(PUMP_PIN, LOW);
 }
 
@@ -136,15 +269,7 @@ boolean writeToSDCard(String fileName, String dataToWrite)
   if (dataFile) {
     dataFile.println(dataToWrite);
     dataFile.close();
-    // print to the serial port too:
-    Serial.println(dataToWrite);
+    return true;
   }
-  // if the file isn't open, pop up an error:
-  else {
-    Serial.println("error opening" + fileName);
-    return false;
-  }
-  return true;
+  return false;
 }
-
-
